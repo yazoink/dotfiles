5a60d14349731b9fc10cd5af14b7d09978eff637
yazoink <yazoink@firemail.cc>
2024-03-12 21:56:13 +1100

    make installer better

--- install.sh
+++ install.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
-if [[ -z "$HOME/.config/vesktop" ]]; then
-    mkdir -p "$HOME/.config/vesktop"
-fi
-if [[ -z "$HOME/.config/vesktop" ]]; then
-    mkdir "$HOME/pic"
+create_missing_dir() {
+    if [[ -z "$1" ]]; then
+        mkdir -p "$1"
+        if [[ $? != 0 ]]; then
+            printf "Installation failed, could not create directory $1. Make sure you have permission to do so.\n"
+        fi
+    fi
+}
+
+stow_dir() {
+    stow "$1" -t ~
+    if [[ $? != 0 ]]; then
+        printf "Installation failed, could not stow %s. Check for any conflicting files.\n" "$1"
+        exit 1
+    fi
+}
+
+usage() {
+    printf "usage: ./install.sh <option>
+    options:
+    \tcyberia - laptop setup (ThinkPad T430 - 1600x900 - Artix runit)
+    \tfluoride - desktop setup (1440p - Arch)\n"
+    exit 1
+}
+
+if [[ "$1" != "cyberia" ]] && [[ "$1" != "fluoride" ]]; then
+    usage
 fi
 
-case $1 in
-    cyberia) stow cyberia -t ~;;
-    fluoruide) stow fluoride -t ~;;
-    *) printf "usage: ./install.sh <option>\noptions:\n\tcyberia - laptop setup (ThinkPad T430 - 1600x900 - Artix runit)\n\tfluoride - desktop setup (1440p - Arch)\n";;
-esac
+create_missing_dir "$HOME/.config/vesktop"
+create_missing_dir "$HOME/.config/jamesdsp"
+create_missing_dir "$HOME/pic"
 
-stow shared -t ~
+stow_dir "$1"
+stow_dir "shared"
+printf "done!\n"
+exit 0
